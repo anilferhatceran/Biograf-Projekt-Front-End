966a486f73b3b208e0eee427e7ef21bd10bebe0d
Fatso62 <39982808+Fatso62@users.noreply.github.com>
2020-09-10 22:03:12 +0200

    Contact-us component made

--- BiografProjekt/biografprojekt/src/app/app.module.ts
+++ BiografProjekt/biografprojekt/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
