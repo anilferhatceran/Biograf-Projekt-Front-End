fee9c51a91340c62b3c4b120e1d0d157be159835
Fatso62 <39982808+Fatso62@users.noreply.github.com>
2020-09-13 16:14:51 +0200

    A lot of components made along with routes and simple html/css

--- BiografProjekt/biografprojekt/src/app/app-routing.module.ts
+++ BiografProjekt/biografprojekt/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { MoviesComponent } from './Components/movies/movies.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AboutUsComponent} from './Components/about-us/about-us.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ContactUsComponent,MoviesComponent, HeaderComponent, AboutUsComponent, FooterComponent]
--- BiografProjekt/biografprojekt/src/app/app.module.ts
+++ BiografProjekt/biografprojekt/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactUsComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
